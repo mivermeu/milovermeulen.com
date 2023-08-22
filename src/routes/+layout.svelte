a5a6231054421dc4a6dc09ed3260f020ddd5fc9a
Milo Vermeulen <milo.vermeulen@stcorp.nl>
2023-08-21 20:51:49 -0500

    Only apply previously common style to homepage

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,28 +2,4 @@
     import './styles.scss';
 </script>
 
-<div class="app">
-    <main>
-        <slot />
-    </main>
-</div>
-
-<style>
-    .app {
-        display: flex;
-        flex-direction: column;
-        min-height: 100vh;
-    }
-
-    main {
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        padding-left: 30px;
-        padding-right: 30px;
-        margin: auto;
-        width: 100%;
-        max-width: 1080px;
-        box-sizing: border-box;
-    }
-</style>
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<script lang='ts'>
     import Hero from '$lib/sections/Hero.svelte';
     import Experience from '$lib/sections/Experience.svelte';
     import Projects from '$lib/sections/Projects.svelte';
@@ -7,12 +7,34 @@
 
 <svelte:head>
     <title>Milo Vermeulen</title>
-    <meta name="Milo Vermeulen" content="Personal website" />
+    <meta name='Milo Vermeulen' content='Personal website' />
 </svelte:head>
 
-<div class="container">
-    <Hero />
-    <Experience />
-    <Projects />
-    <Contact />
+<div class='app'>
+    <main>
+        <Hero />
+        <Experience />
+        <Projects />
+        <Contact />
+    </main>
 </div>
+
+<style>
+    .app {
+        display: flex;
+        flex-direction: column;
+        min-height: 100vh;
+    }
+
+    main {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        padding-left: 30px;
+        padding-right: 30px;
+        margin: auto;
+        width: 100%;
+        max-width: 1080px;
+        box-sizing: border-box;
+    }
+</style>
