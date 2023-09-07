7992c07c9da9301aab734211464a8a8d89ba36eb
Milo Vermeulen <milo.vermeulen@stcorp.nl>
2023-09-07 13:15:44 -0500

    Move plot code to own component

---
+++ src/lib/webneut/NeutrinoPlot.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+    import Plot from 'svelte-plotly.js';
+    import type { Layout } from 'svelte-plotly.js';
+    import { oscillation_parameters, x_values, y_values } from './stores';
+    import type { Parameter } from './types';
+
+    $: range_parameter = Object.values($oscillation_parameters).find(function (par: Parameter) {
+        return par.values.length > 1;
+    }) satisfies Parameter | undefined;
+
+    $: nustr = $oscillation_parameters.anti.values[0] > 0 ? '\u03BD' : '\u03BD&#773;' satisfies string;
+    $: fstr = $oscillation_parameters.nu.values[0] == 0 ? 'e' : $oscillation_parameters.nu.values[0] == 1 ? '\u03BC' : '\u03C4' satisfies string;
+
+    $: layout = {
+        font: { family: 'serif', size: 16 },
+        xaxis: { title: { text: range_parameter ? range_parameter.label : '', standoff: 15 } },
+        title: {
+            text: 'P(' + nustr + '<sub>' + fstr + '</sub>' + '\u2192' + nustr + '<sub>x</sub>)',
+            x: 0,
+            xanchor: 'left',
+            yanchor: 'bottom',
+            xref: 'paper',
+            yref: 'paper'
+        },
+        showlegend: true,
+        legend: {
+            orientation: 'h',
+            x: 1,
+            y: 1,
+            xanchor: 'right',
+            yanchor: 'bottom'
+        },
+        margin: {
+            b: 50,
+            t: 20,
+            l: 50,
+            r: 30,
+            pad: 5
+        }
+    } satisfies Partial<Layout>;
+
+    $: data = [
+        {
+            x: $x_values,
+            y: $y_values[0],
+            name: nustr + '<sub>e</sub>',
+            line: {
+                color: 'green'
+            }
+        },
+        {
+            x: $x_values,
+            y: $y_values[1],
+            name: nustr + '<sub>\u03BC</sub>',
+            line: {
+                color: 'blue'
+            }
+        },
+        {
+            x: $x_values,
+            y: $y_values[2],
+            name: nustr + '<sub>\u03C4</sub>',
+            line: {
+                color: 'red'
+            }
+        }
+    ];
+</script>
+
+<Plot {data} {layout} fillParent />
