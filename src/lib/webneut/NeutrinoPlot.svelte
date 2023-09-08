42570d0354b75897fd92da53d9d42cc136c0f654
Milo Vermeulen <milo.vermeulen@stcorp.nl>
2023-09-07 19:24:20 -0500

    Add functionality for ternary plotting

--- src/lib/webneut/NeutrinoPlot.svelte
+++ src/lib/webneut/NeutrinoPlot.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
     import Plot from 'svelte-plotly.js';
-    import type { Layout } from 'svelte-plotly.js';
+    import type { Layout, Data } from 'svelte-plotly.js';
     import { oscillation_parameters, x_values, y_values } from './stores';
-    import type { Parameter } from './types';
+    import { PlotType, type Parameter } from './types';
 
     $: range_parameter = Object.values($oscillation_parameters).find(function (par: Parameter) {
         return par.values.length > 1;
@@ -11,7 +11,7 @@
     $: nustr = $oscillation_parameters.anti.values[0] > 0 ? '\u03BD' : '\u03BD&#773;' satisfies string;
     $: fstr = $oscillation_parameters.nu.values[0] == 0 ? 'e' : $oscillation_parameters.nu.values[0] == 1 ? '\u03BC' : '\u03C4' satisfies string;
 
-    $: layout = {
+    $: linear_layout = {
         font: { family: 'serif', size: 16 },
         xaxis: { title: { text: range_parameter ? range_parameter.label : '', standoff: 15 } },
         title: {
@@ -39,7 +39,38 @@
         }
     } satisfies Partial<Layout>;
 
-    $: data = [
+    function makeTernAxis(title: string, tickangle: number) {
+        return {
+            title: title,
+            titlefont: { size: 20 },
+            tickangle: tickangle,
+            tickfont: { size: 15 },
+            tickcolor: "rgba(0,0,0,0)",
+            ticklen: 5,
+            showline: true,
+            showgrid: true,
+        };
+    }
+
+    $: ternary_layout = {
+        ternary: {
+            sum: 1,
+            aaxis: makeTernAxis(nustr + "<sub>e</sub>", 0),
+            baxis: makeTernAxis(nustr + "<sub>\u03BC</sub>", 45),
+            caxis: makeTernAxis(nustr + "<sub>\u03C4</sub>", -45),
+        },
+        margin: {
+            l: 40,
+            r: 40,
+            b: 50,
+            t: 50,
+        },
+        font: {
+            family: "serif"
+        }
+    } satisfies Partial<Layout>
+
+    $: linear_data = [
         {
             x: $x_values,
             y: $y_values[0],
@@ -64,7 +95,24 @@
                 color: 'red'
             }
         }
-    ];
+    ] satisfies Data[];
+
+    $: ternary_data = [{
+        type: "scatterternary",
+        mode: "lines",
+        // It would be extremely nice to have a colour gradient in this line, but this is currently impossible in Plotly.
+        // line: {
+        //   color: $x_values
+        // },
+
+        // @ts-ignore: Unrecognised elements.
+        a: $y_values[0],
+        b: $y_values[1],
+        c: $y_values[2],
+    }] satisfies Data[];
+
+    $: data = $oscillation_parameters.plot_type.values[0] === PlotType.Linear? linear_data: ternary_data satisfies Data[];
+    $: layout = $oscillation_parameters.plot_type.values[0] === PlotType.Linear? linear_layout: ternary_layout;
 </script>
 
 <Plot {data} {layout} fillParent />
