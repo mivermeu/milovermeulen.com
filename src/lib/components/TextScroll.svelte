ea18829519845525fa4a973b4ccdf25421adca3b
Milo Vermeulen <milo.vermeulen@stcorp.nl>
2023-09-20 14:52:50 -0500

    Remove unused code

--- src/lib/components/TextScroll.svelte
+++ src/lib/components/TextScroll.svelte
@@ -1,10 +1,8 @@
 <script lang='ts'>
     import { onDestroy, onMount } from 'svelte';
-    import { fly } from 'svelte/transition';
 
     export let strings: [string, ...string[]];
     export let animation_speed: number = 2000;
-    export let animation_distance: number = 20;
 
     // Not necessarily the widest string, but it will do for now.
     $: longest_string = strings.reduce((a, b) => a.length > b.length? a: b) satisfies string;
@@ -27,11 +25,7 @@
 <span class='change-container'>
     {longest_string}
     {#each strings as text, ti}
-        <span
-            class='text-container'
-            style={`top: calc(100% * (${ti} - ${index}));`}
-            in:fly={{y: -animation_distance, duration: animation_speed}} out:fly={{y: animation_distance, duration: animation_speed}}
-        >
+        <span class='text-container' style={`top: calc(100% * (${ti} - ${index}));`}>
             {text}
         </span>
     {/each}
