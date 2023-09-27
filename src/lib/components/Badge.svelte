178ef512c86d44beedf22144950840a7f720262e
Milo Vermeulen <milo.vermeulen@stcorp.nl>
2023-09-26 21:57:07 -0500

    Generalise badge

---
+++ src/lib/components/Badge.svelte
@@ -0,0 +1,28 @@
+<!-- Define a CompanyBadge component that provides additional information about
+    a company when prompted. -->
+
+<script lang='ts'>
+    export let hovering: boolean = false;
+
+    const enter = () => (hovering = true)
+    const leave = () => (hovering = false)
+</script>
+
+<div class='badge' on:mouseenter={enter} on:mouseleave={leave} role='presentation'>
+    <slot />
+</div>
+
+<style lang='scss'>
+    .badge {
+        --badge-padding: 5px;
+        position: relative;
+        padding: var(--badge-padding);
+        margin-right: 7px;
+        margin-bottom: 7px;
+        background-color: var(--color-button);
+        border-radius: var(--border-radius-card);
+
+        font: var(--font-body);
+        color: var(--color-text);
+    }
+</style>
--- src/lib/components/CompanyList.svelte
+++ src/lib/components/CompanyList.svelte
@@ -2,15 +2,30 @@
     in weird places. -->
 
 <script lang='ts'>
-    import CompanyBadge from "./CompanyBadge.svelte";
     import type { Company } from "$lib/utils/types";
+    import { fly } from "svelte/transition";
+    import Badge from "./Badge.svelte";
 
     export let companies: Company[] = []
+
+    let hovering: boolean[] = Array(companies.length).fill(false);
 </script>
 
 <div class='badge-container'>
-    {#each companies as company}
-        <CompanyBadge {company} />
+    {#each companies as company, ci}
+        <Badge bind:hovering={hovering[ci]}>
+            <a on:click|stopPropagation href={company.website} target='_blank'>{company.name}</a>
+            {#if hovering[ci]}
+                <div class='hover-window' transition:fly={{y: 30, x: 0, duration: 100}}>
+                    {#if company.logo}
+                        <img class='hover-image' src={company.logo} alt='{company.name} logo' />
+                    {/if}
+                    <div class='hover-body'>
+                        {company.location.city}, {company.location.country}
+                    </div>
+                </div>
+            {/if}
+        </Badge>
     {/each}
 </div>
 
@@ -18,5 +33,36 @@
     .badge-container {
         display: flex;
         flex-wrap: wrap;
+    }
+
+    .hover-window {
+        pointer-events: none;
+        position: absolute;
+        left: 0;
+        top: calc(var(--font-size-body) + var(--badge-padding) * 2 + var(--border-radius));
+        background-color: var(--color-button);
+        border-radius: var(--border-radius-card);
+        z-index: 100000;
+        overflow: hidden;
+
+        display: flex;
+        flex-direction: column;
+
+        // Work around a Safari bug that causes shadows to be cut off after animating.
+        -webkit-transform: translateZ(0);
+        transform: translateZ(0);
+        filter: var(--shadow);
+
+        .hover-image {
+            flex: 1 1 auto;
+            max-width: 250px;
+            max-height: 100px;
+        }
+
+        .hover-body {
+            padding: var(--badge-padding);
+            white-space: nowrap;
+            flex: 1 1 auto;
+        }
     }
 </style>
