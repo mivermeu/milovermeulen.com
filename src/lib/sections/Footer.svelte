3f7100d6abfc21282336d5731660bd8eb86557aa
Milo Vermeulen <milo.vermeulen@stcorp.nl>
2023-09-28 21:41:47 -0500

    Add sources of inspiration and icon sources

--- src/lib/sections/Footer.svelte
+++ src/lib/sections/Footer.svelte
@@ -8,6 +8,16 @@
     <div>
         © {new Date().getFullYear()} Milo Vermeulen
     </div>
+    <div>
+        Sources of inspiration: 
+        <a href='https://fantinel.dev'>Matt Fantinel</a>
+        <a href='https://brittanychiang.com'>Brittany Chiang</a>
+        <a href='https://olaolu.dev'>Olaolu Olawuyi</a>
+        <a href='https://www.seanhalpin.xyz'>Séan Halpin</a>
+    </div>
+    <div>
+        Icons by <a href='https://iconoir.com'>Iconoir</a>, <a href='https://www.svgrepo.com'>SVG repo</a> and myself.
+    </div>
     <div class='socials-container'>
         <Socials />
     </div>
@@ -16,7 +26,7 @@
 <style lang='scss'>
     .footer {
         width: 100%;
-        height: 200px;
+        height: 250px;
         margin-top: 100px;
 
         z-index: 2;
@@ -27,6 +37,10 @@
         justify-content: end;
         align-content: center;
         text-align: center;
+    }
+
+    a {
+        pointer-events: all;
     }
 
     .socials-container {
