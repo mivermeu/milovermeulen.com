175ab40ad1f4511c44913e332a168d207f588980
Milo Vermeulen <milo.vermeulen@stcorp.nl>
2023-09-29 22:40:50 -0500

    Rework hex grid to handle width change

--- src/lib/components/HexGrid.svelte
+++ src/lib/components/HexGrid.svelte
@@ -3,6 +3,7 @@
 <script lang='ts'>
     import { onMount, type SvelteComponent } from 'svelte';
     import Hexagon from './Hexagon.svelte';
+    import { writable } from 'svelte/store';
 
     export let hex_width: number = 45;
     export let top: number | undefined = undefined;
@@ -11,44 +12,49 @@
     export let grid_height: number = 600;
     export let transition_speed: number = 800;
 
+    let timer = 0;
+    const start_time: number = Date.now()
+
     let innerWidth: number = 0;
-    $: horizontal_distance = hex_width * 3 satisfies number;
-    $: vertical_distance = hex_width * Math.sqrt(3) satisfies number;
+    $: horizontal_distance = hex_width * 3 + horizontal_gap satisfies number;
+    $: vertical_distance = (hex_width * Math.sqrt(3) + vertical_gap) / 2 satisfies number;
     const vertical_gap: number = horizontal_gap / Math.sqrt(3);
 
-    $: num_cols = innerWidth / horizontal_distance satisfies number;
-    $: num_rows = grid_height / (vertical_distance + vertical_gap) + 1 satisfies number;
+    $: num_cols = Math.floor(innerWidth / horizontal_distance) + 2 satisfies number;
+    $: num_rows = Math.floor(grid_height / (vertical_distance + vertical_gap)) + 6 satisfies number;
     const x_start: number = -hex_width / 2;
     const y_start: number = bottom === undefined? -3 * hex_width: hex_width;
 
     let hexagons: SvelteComponent[] = [];
-    let pulse_interval: number = 0;
+    $: hexagons = hexagons.filter((hexagon) => hexagon !== null)  // When changing the screen, sometimes null hexagons are left over.
+    let interval: number = 0;
 
-    const hex_sustain: number = 500;
+    const hex_sustain: number = 700;
     const pulse_delay: number = 6000;
-    const pulse_propagation_delay: number = 200;
+    const pulse_propagation_delay: number = 150;
 
     onMount(() => {
-        setTimeout(() => vertical_down_grid_pulse(), 200);
-        pulse_interval = window.setInterval(() => {
-            vertical_down_grid_pulse();
-        }, pulse_delay);
+        interval = window.setInterval(() => {
+            timer = Date.now() - start_time;
+        }, 100);
 
-        return () => window.clearInterval(pulse_interval);
+        return () => window.clearInterval(interval);
     });
 
-    function vertical_down_grid_pulse(): void {
-        for(let ri = 0; ri < num_rows; ri++) {
-            [0, 0.5].forEach((row_offset) => {
-                [0, hex_sustain].forEach((time_in_pulse) => {
-                    setTimeout(() => {
-                        for(let ci = 0; ci < num_cols; ci++) {
-                            hexagons[ri * num_cols + ci * 2 + row_offset * 2].raised = time_in_pulse === 0? true: false;
-                        }
-                    }, (ri + row_offset) * pulse_propagation_delay + time_in_pulse);
-                })
-            });
-        }
+    let last_pulse_time: number = -pulse_delay;
+
+    $: if(timer - last_pulse_time > pulse_delay) {
+        last_pulse_time += pulse_delay;
+
+        hexagons.forEach((hexagon, hex_index) => {
+            const row_index: number = Math.floor(hex_index / num_cols);
+            setTimeout(() => {
+                hexagon.raised = true;
+            }, row_index * pulse_propagation_delay);
+            setTimeout(() => {
+                hexagon.raised = false;
+            }, row_index * pulse_propagation_delay + hex_sustain);
+        })
     }
 </script>
 
@@ -57,19 +63,18 @@
 <div
     class='hex-grid'
     style='--hex-width: {hex_width}px; --grid-height: {grid_height}px; --top: {top}px; --bottom: {bottom}px;'>
-    {#each {length: num_rows} as _, ri}
-        {#each {length: num_cols} as _, ci}
-            {#each [0, 0.5] as offset, oi}
-                {@const x = x_start + (ci + offset) * (horizontal_distance + horizontal_gap)}
-                {@const y = y_start + (ri + offset) * (vertical_distance + vertical_gap)}
-                <Hexagon
-                    bind:this={hexagons[ri * num_cols + ci * 2 + oi]}
-                    width={hex_width}
-                    {x}
-                    {y}
-                    {transition_speed} />
-            {/each}
-        {/each}
+    {#each {length: num_rows * num_cols} as _, hex_index}
+        {@const row_index = Math.floor(hex_index / num_cols)}
+        {@const column_index = hex_index % num_cols}
+        {@const h_offset = (row_index % 2) / 2}
+        {@const x = x_start + (column_index + h_offset) * horizontal_distance}
+        {@const y = y_start + row_index * vertical_distance}
+        <Hexagon
+            bind:this={hexagons[hex_index]}
+            width={hex_width}
+            {x}
+            {y}
+            {transition_speed} />
     {/each}
 </div>
 
