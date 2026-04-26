9dd08373cf550f486551bfd3eec56d8695bd747d
Milo Vermeulen <milovermeulen@me.com>
2026-04-26 17:15:57 -0400

    Center content, separate main from background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,21 +9,27 @@
     import Education from '$lib/sections/Education.svelte';
 </script>
 
-<div class="container mx-auto flex h-screen px-4 sm:px-6 lg:px-8">
-    <BracketedSection className="w-1/3 h-screen border-r-3 border-black">
+<div class="container mx-auto flex h-screen justify-center px-4 sm:px-6 lg:px-8">
+    <BracketedSection className="w-1/3 h-screen">
         <div
             class="pointer-events-none absolute inset-0 z-20 shadow-[inset_-4px_0_1px_rgba(255,255,255,0.8)]"
         ></div>
         <div
-            class="pointer-events-none absolute inset-0 z-20 shadow-[inset_-3px_0_1px_rgba(0,0,0,0.8)]"
+            class="pointer-events-none absolute inset-0 z-20 shadow-[inset_-3px_0_1px_rgba(0,0,0,1)]"
         ></div>
         <div class="h-full p-6 pl-12">
             <Aside />
         </div>
     </BracketedSection>
 
-    <main class="z-10 w-2/3 overflow-y-auto bg-[#222]">
+    <main class="z-10 max-w-prose overflow-y-auto bg-[#222]">
         <div class="relative p-8">
+            <div
+                class="pointer-events-none absolute inset-0 z-20 shadow-[inset_-4px_0_1px_rgba(255,255,255,0.8)]"
+            ></div>
+            <div
+                class="pointer-events-none absolute inset-0 z-20 shadow-[inset_-3px_0_1px_rgba(0,0,0,1)]"
+            ></div>
             <Noise />
             <About />
             <Experience />
