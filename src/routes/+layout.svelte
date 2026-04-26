8d770042e06e4f94d9ee3dfa6263f4c2ca6a6999
Milo Vermeulen <milovermeulen@me.com>
2026-04-26 10:53:54 -0400

    Continue noise pattern off the main content

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import '../app.css';
     import favicon from '$lib/assets/favicon.svg';
+    import Noise from '$lib/components/Noise.svelte';
 
     let { children } = $props();
 </script>
@@ -10,9 +11,10 @@
 <div class="relative min-h-screen w-full overflow-x-hidden bg-[#222] text-white">
     <!-- Global glow -->
     <div
-        class="pointer-events-none fixed top-0 left-full z-0 h-[300%] w-[400%] -translate-x-1/2 -translate-y-1/2 rounded-full
-        bg-radial from-white/10 via-transparent to-transparent blur-[120px]"
+        class="pointer-events-none fixed top-0 left-full z-100 h-[300%] w-[400%] -translate-x-1/2 -translate-y-1/2 rounded-full
+        bg-radial from-white/20 via-transparent to-transparent blur-[120px]"
     ></div>
+    <Noise />
 
     {@render children()}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@
     import Education from '$lib/sections/Education.svelte';
 </script>
 
-<div class="flex h-screen w-screen">
+<div class="container mx-auto flex h-screen px-4 sm:px-6 lg:px-8">
     <BracketedSection className="w-1/3 h-screen border-r-3 border-black">
         <div
             class="pointer-events-none absolute inset-0 z-20 shadow-[inset_-4px_0_1px_rgba(255,255,255,0.8)]"
@@ -17,13 +17,12 @@
         <div
             class="pointer-events-none absolute inset-0 z-20 shadow-[inset_-3px_0_1px_rgba(0,0,0,0.8)]"
         ></div>
-        <Noise />
         <div class="h-full p-6 pl-12">
             <Aside />
         </div>
     </BracketedSection>
 
-    <main class="w-2/3 overflow-y-auto">
+    <main class="z-10 w-2/3 overflow-y-auto bg-[#222]">
         <div class="relative p-8">
             <Noise />
             <About />
