db87e83f43de2a59c09cb7c37966981860a48187
Milo Vermeulen <milovermeulen@me.com>
2026-04-27 19:00:22 -0400

    Leave out bachelor/master

--- src/lib/sections/Education.svelte
+++ src/lib/sections/Education.svelte
@@ -1,11 +1,11 @@
 <script>
-    import { bachelor, master, phd } from '$lib/data/education';
+    import { phd } from '$lib/data/education';
     import { month_plus_year } from '$lib/utils/datetime';
 </script>
 
 <section id="education" class="mb-16">
     <div class="space-y-4">
-        {#each [phd, master, bachelor] as education, index (index)}
+        {#each [phd] as education, index (index)}
             <div class="border-l-2 border-white/20 pl-4">
                 <h3 class="text-lg font-medium text-white">{education.name.toLowerCase()}</h3>
                 <p class="text-sm text-white/50">
