dea6d212d0ec991ad019b32cd4d67193e90d8d95
Milo Vermeulen <milovermeulen@me.com>
2026-04-30 22:37:24 -0400

    Add experience start/stop to side of experience item

--- src/lib/components/ExperienceItem.svelte
+++ src/lib/components/ExperienceItem.svelte
@@ -14,15 +14,20 @@
     const job_stop_text = $derived(job.stop ? month_plus_year(job.stop) : 'Current');
 </script>
 
-<div class="border-l border-white pl-4 {className}">
-    <h3 class="text-lg font-medium text-white">{job.title.toLowerCase()}</h3>
-    <p class="text-sm">
-        <span class="my-2 flex items-center gap-1">
-            {#each job.companies as company (company.name)}
-                <InlineButton title={company.name} href={company.website} />
-            {/each}
-            · {job_start_text} - {job_stop_text}
-        </span>
-    </p>
-    <p>{job.description}</p>
+<div class="grid grid-cols-[auto_1fr] gap-4 {className}">
+    <div class="flex flex-col justify-between text-right text-xs whitespace-nowrap">
+        <span>{job_stop_text}</span>
+        <span>{job_start_text}</span>
+    </div>
+    <div class="border-l border-white pl-4">
+        <h3 class="text-lg font-medium text-white">{job.title.toLowerCase()}</h3>
+        <p class="text-sm">
+            <span class="my-2 flex items-center gap-1">
+                {#each job.companies as company (company.name)}
+                    <InlineButton title={company.name} href={company.website} />
+                {/each}
+            </span>
+        </p>
+        <p>{job.description}</p>
+    </div>
 </div>
