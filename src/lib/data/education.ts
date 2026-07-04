e1de4af8c2787b2f721fa25d1055981ba6277aa6
Milo Vermeulen <milovermeulen@me.com>
2026-07-03 23:31:24 -0400

    Remove dead code and dependencies

--- src/lib/data/education.ts
+++ src/lib/data/education.ts
@@ -1,15 +1,5 @@
-// List of educations received.
-
 import type { EducationEntry } from '$lib/utils/types';
 import { nikhef, ru, goteborg, felix, cern, uh } from '$lib/data/companies';
-
-// import phd_thesis_cover from '$lib/images/covers/phd_thesis_cover.svg'
-// import master_thesis_cover from '$lib/images/covers/master_thesis_cover.svg'
-// import bachelor_thesis_cover from '$lib/images/covers/bachelor_thesis_cover.svg'
-
-// import phd_thesis from '$lib/documents/A_Blessing_in_Disguise.pdf'
-// import master_thesis from '$lib/documents/Calcium_Enrichment_for_the_Study_of_Neutrinoless_Double_Beta_Decay.pdf'
-// import bachelor_thesis from '$lib/documents/The_Scanning_Problem_of_FLARE_and_THz_Pulse_Slicing.pdf'
 
 export const phd: EducationEntry = {
     companies: [nikhef, cern, uh],
@@ -23,8 +13,6 @@ export const phd: EducationEntry = {
     `,
     thesis: {
         name: 'A Blessing in Disguise - Characterisations of ProtoDUNE photon showers for neutrino measurements in DUNE'
-        // link: phd_thesis,
-        // image: phd_thesis_cover
     }
 };
 
@@ -40,8 +28,6 @@ export const master: EducationEntry = {
     `,
     thesis: {
         name: 'Calcium Enrichment for the Study of Neutrinoless Double Beta Decay'
-        // link: master_thesis,
-        // image: master_thesis_cover
     }
 };
 
@@ -58,7 +44,5 @@ export const bachelor: EducationEntry = {
     `,
     thesis: {
         name: 'The Scanning Problem of FLARE and THz Pulse Slicing'
-        // link: bachelor_thesis,
-        // image: bachelor_thesis_cover
     }
 };
