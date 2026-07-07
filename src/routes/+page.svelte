1db0f95aa4c23d3b6bb9e12b942edc7d967cdd73
Milo Vermeulen <milovermeulen@me.com>
2026-07-06 22:17:58 -0400

    Add indent around mobile dials

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -54,11 +54,11 @@
 
     <!-- Mobile footer -->
     <footer class="flex h-40 shrink-0 items-center justify-center gap-2 bg-brand-bg py-7 lg:hidden">
-        <div class="aspect-square h-full">
+        <div class="aspect-square h-full rounded-full shadow-indent">
             <SectionDial />
         </div>
         <Contact />
-        <div class="aspect-square h-full">
+        <div class="aspect-square h-full rounded-full shadow-indent">
             <ScrollDial />
         </div>
     </footer>
